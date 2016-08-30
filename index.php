9921b3405e0e844966877e3fe7fd1d3bc08ec258
M Roderick <aws910@gmail.com>
2016-08-30 14:08:22 -0700

    removed unnecessary dependency

--- index.php
+++ index.php
@@ -82,9 +82,6 @@ if(isset($_POST['search_eng'])){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script type="text/javascript"
-   src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
-  </script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
